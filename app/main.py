9126acd15a8e4d5fccc44c07c1954f749913a465
raffanammar <muhamad.adiyansyah@limabit.com>
2026-09-23 10:09:24 +0700

    Dockerize Flask app and connect with PostgreSQL via docker compose

--- app/main.py
+++ app/main.py
@@ -16,8 +16,9 @@ def create_app():
     db_name = os.getenv("POSTGRES_DB")
     db_port = os.getenv("POSTGRES_PORT", "5432")
 
+    db_host = os.getenv("DB_HOST", "localhost")
     app.config["SQLALCHEMY_DATABASE_URI"] = (
-        f"postgresql://{db_user}:{db_password}@localhost:{db_port}/{db_name}"
+        f"postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
     )
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
